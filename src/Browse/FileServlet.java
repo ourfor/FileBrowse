ba7100509c3c3af00347062261cb85dee596f334
Sagit <2320813747@qq.com>
2019-05-02 20:23:54 +0800

    when we use proxy to deploy tomcat webapp, need special solution

--- src/Browse/FileServlet.java
+++ src/Browse/FileServlet.java
@@ -19,7 +19,8 @@ import javax.servlet.http.HttpServletResponse;
 )
 public class FileServlet extends HttpServlet {
         private static final long serialVersionUID = 1L;
-        private static String BaseUrl = "/Users/sagit";
+        private static String BaseUrl = "/var/www/file";
+        private static String ProjectName = "FileBrowse/";
         protected void doGet(HttpServletRequest request,
                              HttpServletResponse response)
                 throws IOException,ServletException{
@@ -28,16 +29,23 @@ public class FileServlet extends HttpServlet {
                 response.setCharacterEncoding("UTF-8");
                 response.setContentType("text/html;charset=UTF-8");
 
-                StringBuffer url = request.getRequestURL();
+                StringBuffer url_ori = request.getRequestURL();
+                System.out.println("原请求的URL为:"+url_ori);
+                String url_dirty = url_ori.toString();
+                String url = url_dirty.replaceFirst("FileBrowse/","");
+
+                System.out.println("处理以后的请求URL为:"+url);
                 int i =0;
-                for(int j=0;j<4&&i<url.length();i++) {
+                for(int j=0;j<3&&i<url.length();i++) {
                     if(url.charAt(i)=='/') j++;
                 }
                 String dir = url.substring(i-1);
                 dir = dir.replaceAll("%20","\\ ");
+                dir = dir.replaceAll(" ","\\ ");
                 String server = url.substring(0,i);
 
                 File type = new File(BaseUrl+dir);
+                System.out.println("查看路径:"+BaseUrl+dir+"的文件类型");
                 if(type.isDirectory()){
                     Show(dir,server,response);
                 }
@@ -69,7 +77,7 @@ public class FileServlet extends HttpServlet {
             list = getChildren(BaseUrl+dir);
             System.out.println("当前请求文件为:"+BaseUrl+dir);
 
-            out.println("<!DOCTYPE HTML><head><title>"+dir+"</title></head><body>");
+            out.println("<!DOCTYPE HTML><head><title>"+dir.replaceFirst(ProjectName,"")+"</title></head><body>");
 //                out.println("目录:"+dir+"<br>");
 //                out.println("服务器:"+server+"<br>");
 
@@ -87,8 +95,12 @@ public class FileServlet extends HttpServlet {
         }
 
         private static void Download(String dir,HttpServletResponse response) throws IOException {
+            dir = dir.replaceFirst(ProjectName,"");
+            dir = dir.replaceAll(" ","\\ ");
+
             String filename = dir.substring(dir.lastIndexOf("/")+1);
-            System.out.println(dir);
+            dir = BaseUrl + dir;
+            System.out.println("请求的文件路径为:"+dir);
             response.setContentType("application/zip");
             response.setHeader("Content-Disposition","attachment;filename="+filename);
             OutputStream os = response.getOutputStream();
